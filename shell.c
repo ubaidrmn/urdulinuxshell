ed73ed989c31d42e5197818271f6ae472390588d
ubaidrmn <rehmanubaid2003@gmail.com>
2024-03-12 16:38:46 +0500

    feat: define function for extracting words from a command

--- shell.c
+++ shell.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int DEFAULT_COMMAND_SIZE = 20;
+int DEFAULT_COMMAND_WORD_SIZE = 5;
 
 char *read_command();
+char **extract_words(char *command);
 
 int main() {
 
   while (1) {
     char *command;
+    char **command_words;
     printf("ubaidrmn@pc:~/$");
     command = read_command();
-    printf("%.*s",DEFAULT_COMMAND_SIZE,command);
+    command_words = extract_words(command);
+    // printf("%.*s",DEFAULT_COMMAND_SIZE,command);
   }
 
   return 0;
@@ -33,4 +38,23 @@ char *read_command() {
 
     i++;
   }
+}
+
+char **extract_words(char *command) {
+  char **command_words;
+  char *command_word;
+  int i;
+  
+  command_words = malloc(sizeof(char*) * DEFAULT_COMMAND_WORD_SIZE);
+  command_word = strtok(command, " ");
+  i = 0;
+
+  while(command_word) {
+    command_words[i] = command_word;
+    command_word = strtok(NULL, " ");
+
+    i++;
+  }
+
+  return command_words;
 }
