7e5224d3863effbe4ed5a42b3c8bca716a4384b5
ubaidrmn <rehmanubaid2003@gmail.com>
2024-03-13 15:36:52 +0500

    feat: add more commands

--- include/main.h
+++ include/main.h
@@ -11,10 +11,10 @@
 
 #define BADLO_DIR "badlo_dir"
 #define BANAO_DIR "banao_dir"
-#define DIKHAO_DIR "dikhao_dir"
 #define URAO_DIR "urao_dir"
 #define MOJOODA_DIR "mojooda_dir"
-#define EXIT_SHELL "exit"
+#define SAAF "saaf"
+#define EXIT_SHELL "band"
 // static char *DEFAULT_SUPPORTED_COMMANDS[] = {"badlo_dir", "banao_dir", "dikhao_dir", "urao_dir", "mojooda_dir"};
 
 // Function definitions
--- src/execution.c
+++ src/execution.c
@@ -1,15 +1,24 @@
 #include "../include/main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
 
 void execute_command(char **tokens) {
     char *token_0 = tokens[0];
+    char current_dir[100];
+
     if (strcmp(token_0, BADLO_DIR) == 0) {
         chdir(tokens[1]);
     } else if (strcmp(token_0, BANAO_DIR) == 0) {
         mkdir(tokens[1], 0775);
+    } else if (strcmp(token_0, URAO_DIR) == 0) {
+        rmdir(tokens[1]);
+    } else if (strcmp(token_0, MOJOODA_DIR) == 0) {
+        printf("%s\n", getcwd(current_dir, 100));
+    } else if (strcmp(token_0, SAAF) == 0) {
+        system("clear");
     } else if (strcmp(token_0, EXIT_SHELL) == 0) {
         exit(EXIT_SUCCESS);
     } else {
